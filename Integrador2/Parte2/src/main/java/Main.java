b340084517cb01a7f4cad454b1df3f074834b4b1
augustomtt <65096407+augustomtt@users.noreply.github.com>
2021-11-10 18:25:17 -0300

    Se genera archivo con resultados de los 3 canales
    Se genera archivo con resultados de los 3 canales

--- Integrador2/Parte2/src/main/java/Main.java
+++ Integrador2/Parte2/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        PrintStream out = new PrintStream(new FileOutputStream("salidaCanales.txt"));
+        System.setOut(out);
+
        // Cambiar datos de n, m entrada, y matriz.
         int n = 2,m = 3;
         double[] entrada = new double[n];
@@ -13,6 +23,65 @@ public class Main {
         matriz[1][2] =0.6666;
         double[] salida = probSalida(matriz,entrada,m);
 
+        //-----------------------
+        int n1 = 5, m1= 3;
+        int n2 = 4, m2= 4;
+        int n3 = 6, m3= 4;
+        double[] entrada1 = {0.1,0.2,0.2,0.3,0.2};
+        double[] entrada2 = {0.1,0.3,0.2,0.4};
+        double[] entrada3 = {0.1,0.2,0.1,0.3,0.2,0.1};
+        double[] salida1;
+        double[] salida2;
+        double[] salida3;
+        double[][] matrizS;
+
+        double[][] canal1 = {{0.3,0.3,0.4},{0.4,0.4,0.2},{0.3,0.3,0.4},{0.3,0.4,0.3},{0.3,0.4,0.3}};
+        double[][] canal2 = {{0.2,0.3,0.2,0.3},{0.3,0.3,0.2,0.2},{0.3,0.2,0.2,0.3},{0.3,0.3,0.3,0.1}};
+        double[][] canal3 = {{0.2,0.3,0.2,0.3},{0.3,0.3,0.3,0.1},{0.2,0.2,0.3,0.3},{0.3,0.3,0.2,0.2},{0.2,0.3,0.3,0.2},{0.2,0.3,0.3,0.2}};
+
+        System.out.println("========== CANAL 1 ==========");
+        salida1 = probSalida(canal1,entrada1,m1);
+        for(int i=0;i<salida1.length;i++) {
+            System.out.print("P(b"+i+") = "+salida1[i]+ "    ");
+        }
+        System.out.println();
+        System.out.println("Equivocacion H(A/B) = "+equivocacion(canal1,entrada1,salida1));
+        System.out.println("Informacion Mutua I(A/B) = "+informacionMutua(canal1,entrada1,salida1));
+        matrizS = matrizSalida(canal1,entrada1,salida1);
+        System.out.println("Equivocacion: H(B/A) =  "+equivocacion(matrizS,salida1,entrada1));
+        System.out.println("Informacion Mutua: I(B/A) = "+informacionMutua(matrizS,salida1,entrada1));
+        System.out.println();
+
+
+        System.out.println("========== CANAL 2 ==========");
+        salida2 = probSalida(canal2,entrada2,m2);
+        for(int i=0;i<salida2.length;i++) {
+            System.out.print("P(b"+i+") = "+salida2[i]+ "    ");
+        }
+        System.out.println();
+        System.out.println("Equivocacion H(A/B) = "+equivocacion(canal2,entrada2,salida2));
+        System.out.println("Informacion Mutua I(A/B) = "+informacionMutua(canal2,entrada2,salida2));
+        matrizS = matrizSalida(canal2,entrada2,salida2);
+        System.out.println("Equivocacion: H(B/A) =  "+equivocacion(matrizS,salida2,entrada2));
+        System.out.println("Informacion Mutua: I(B/A) = "+informacionMutua(matrizS,salida2,entrada2));
+        System.out.println();
+
+
+        System.out.println("========== CANAL 3 ==========");
+        salida3 = probSalida(canal3,entrada3,m3);
+        for(int i=0;i<salida3.length;i++) {
+            System.out.print("P(b"+i+") = "+salida3[i]+ "    ");
+        }
+        System.out.println();
+        System.out.println("Equivocacion H(A/B) = "+equivocacion(canal3,entrada3,salida3));
+        System.out.println("Informacion Mutua I(A/B) = "+informacionMutua(canal3,entrada3,salida3));
+        matrizS = matrizSalida(canal3,entrada3,salida3);
+        System.out.println("Equivocacion: H(B/A) =  "+equivocacion(matrizS,salida3,entrada3));
+        System.out.println("Informacion Mutua: I(B/A) = "+informacionMutua(matrizS,salida3,entrada3));
+        System.out.println();
+    }
+        //--------------------
+        /*
         for(int i=0;i<salida.length;i++) {
             System.out.println("P(b="+i+"): "+salida[i]);
         }
@@ -22,8 +91,8 @@ public class Main {
         System.out.println("Informacion Mutua: "+informacionMutua(matriz,entrada,salida));
         double[][] matrizS = matrizSalida(matriz,entrada,salida);
         System.out.println("Equivocacion: "+equivocacion(matrizS,salida,entrada));
-        System.out.println("Informacion Mutua: "+informacionMutua(matrizS,salida,entrada));
-    }
+        System.out.println("Informacion Mutua: "+informacionMutua(matrizS,salida,entrada));*/
+
     public static double[][] matrizSalida(double[][] matriz,double[] entrada,double[] salida){
         double[][] invertida = new double[salida.length][entrada.length];
 
